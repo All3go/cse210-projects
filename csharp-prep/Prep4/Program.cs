42051232d6b31626c5a8d09edad2b0bac514dd6d
Austin Linford <alllego321@gmail.com>
2025-09-25 21:37:32 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Welcome to the Number tracker!");
+        Console.WriteLine("You can enter as many numbers as you like, and when you're done, Enter the Number '0' to see the results.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            double average = (double)sum / numbers.Count;
+            Console.WriteLine($"The sum of the numbers is: {sum}");
+            Console.WriteLine($"The average of the numbers is: {average}");
+            Console.WriteLine($"The largest number is: {numbers.Max()}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
