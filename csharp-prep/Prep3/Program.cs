6965ba7d3a1a46bc0f9a46d5cb2c9ffa13d92adf
Austin Linford <alllego321@gmail.com>
2025-09-25 20:15:19 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Console.WriteLine("Welcome to Number Guesser! Enter a number between 1 and 100 and we will tell you if it's higher or lower.");
+        Random random = new Random();
+        int SecretNumber = random.Next(1, 100);
+
+        Console.Write("Enter your guess: ");
+        int UserGuess = int.Parse(Console.ReadLine());
+
+        while (UserGuess != SecretNumber)
+        {
+            if (UserGuess < SecretNumber)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("Enter your guess: ");
+                UserGuess = int.Parse(Console.ReadLine());
+            }
+            else if (UserGuess > SecretNumber)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("Enter your guess: ");
+                UserGuess = int.Parse(Console.ReadLine());
+            }
+
+            if (UserGuess == SecretNumber)
+            {
+                Console.WriteLine("That is my secret Number! You Win!");
+                Console.Write("Would you like to play again? (y/n): ");
+                string playAgain = Console.ReadLine().ToLower();
+                if (playAgain == "y")
+                {
+                    SecretNumber = random.Next(1, 100);
+                    Console.Write("Enter your guess: ");
+                    UserGuess = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for playing! Goodbye!");
+                    break;
+                }
+            }
+
+        }
+    }     
 }
