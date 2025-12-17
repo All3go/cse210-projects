67f310b09ada0445ef67e76019289e415ce3cdff
Austin Linford <alllego321@gmail.com>
2025-12-17 02:47:45 -0700

    I'm ready to crash out

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,41 @@
+public class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} ({_minutes} min)";
+    }
+
+    protected int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    protected string GetDate()
+    {
+        return _date;
+    }
+}
